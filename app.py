05c00ecb9f101157cc90c8f274d0cfcad6f8270f
Shashwat Tewari <122259612+tewarishash12@users.noreply.github.com>
2025-02-21 12:15:45 +0000

    made minor updates

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask
 import os
 import datetime
-import psutil
+import getpass
 
 app = Flask(__name__)
 
 def get_top_output():
     return os.popen("top -b -n 1").read()
 
+def get_system_username():
+    return getpass.getuser()
+
 @app.route('/htop')
 def htop():
-    username = os.getenv("USER") or os.getenv("USERNAME")
+    fullName = "Shashwat Tewari"
+    system_username = get_system_username()
     server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S IST")
     top_output = get_top_output()
 
     return f"""
-    <h1>Name - Shashwat Tewari</h1>
-    <h2>Username - {username}</h2>
+    <h1>Name - {fullName}</h1>
+    <h2>User - {system_username}</h2>
     <h2>Server Time (IST) - {server_time}</h2>
     <pre>{top_output}</pre>
     """
